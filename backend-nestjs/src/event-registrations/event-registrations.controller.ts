cea82b72e865cb43961419dbc0b7ad8af1b626fa
Superherocode <213042559+NguyenDung0101@users.noreply.github.com>
2025-07-23 17:02:58 +0700

    bỏ backend

--- backend-nestjs/src/event-registrations/event-registrations.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('event-registrations')
-export class EventRegistrationsController {}
--- backend-nestjs/src/event-registrations/event-registrations.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EventRegistrationsController } from './event-registrations.controller';
-import { EventRegistrationsService } from './event-registrations.service';
-
-@Module({
-  controllers: [EventRegistrationsController],
-  providers: [EventRegistrationsService]
-})
-export class EventRegistrationsModule {}
